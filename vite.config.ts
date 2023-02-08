9fcd76fbdccfa18903b70bd6ce73044da22051f2
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-02-09 00:15:08 +0500

    feat: completed setup front side, cleared project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
+import { AliasOptions, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tscPlugin from 'vite-plugin-tsc'
+
+import path from 'path'
+const getAlias = (aliases: string[]): AliasOptions =>
+  aliases.map((alias) => ({
+    find: alias,
+    replacement: path.resolve(__dirname, 'src', alias),
+  }))
+
+const alias: AliasOptions = getAlias([
+  'components',
+  'types',
+  'assets',
+  'pages',
+  'api',
+  'constants',
+  'utils',
+  'helpers',
+  'state',
+  'router',
+  'interfaces',
+])
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tscPlugin()],
+  resolve: {
+    alias,
+  },
 })
