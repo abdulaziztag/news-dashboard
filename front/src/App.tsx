a35cccf5664bf7aee5b7cd01aa21e6320600a544
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-02-13 02:01:01 +0500

    feat: completed SignUp and confirmation part of integrating

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,30 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from 'router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css'
+
 function App() {
+  const queryClient = new QueryClient()
+
   return (
     <div className="h-full">
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable={false}
+          pauseOnHover
+          theme="light"
+        />
+      </QueryClientProvider>
     </div>
   )
 }
