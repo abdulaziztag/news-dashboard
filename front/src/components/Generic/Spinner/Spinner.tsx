172f2cfb5eac71c00604378c3bbce9b34f55d06e
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-02-14 07:39:09 +0500

    fix: added props to spinner

--- front/src/components/Generic/Spinner/Spinner.tsx
+++ front/src/components/Generic/Spinner/Spinner.tsx
@@ -1,9 +1,14 @@
-export const Spinner = () => {
+import cx from 'classnames'
+
+export const Spinner = ({ className }: { className?: string }) => {
   return (
     <div role="status">
       <svg
         aria-hidden="true"
-        className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-500 fill-white"
+        className={cx(
+          'w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-500 fill-white',
+          className
+        )}
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
