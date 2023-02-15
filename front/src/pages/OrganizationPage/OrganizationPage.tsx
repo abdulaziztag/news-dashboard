67de52948c5c34cdfaf085120781564b3793db2e
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-02-16 02:29:35 +0500

    feat: completed integration in organization page

--- front/src/pages/OrganizationPage/OrganizationPage.tsx
+++ front/src/pages/OrganizationPage/OrganizationPage.tsx
@@ -1,7 +1,89 @@
 import { useParams } from 'react-router-dom'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { getOrganizationById } from 'api/organization'
+import { useState } from 'react'
+import { Organization } from 'interfaces'
+import { ClipLoader } from 'react-spinners'
+import { colors } from 'constants/colors'
+import { toast } from 'react-toastify'
+import { getSubscriptions, subscribe, unsubscribe } from 'api/user'
+import { Button } from 'components/Generic/Button'
 
 export const OrganizationPage = () => {
   const { organizationId } = useParams()
+  const [organization, setOrganization] = useState<Organization>()
+  const [subscriptions, setSubscriptions] = useState<string[]>([])
+  const [isSubscribed, setIsSubscribed] = useState<boolean>(false)
 
-  return <>{organizationId}</>
+  const subsQuery = useQuery({
+    queryKey: ['subscriptions'],
+    queryFn: getSubscriptions,
+    onSuccess: (data) => {
+      setSubscriptions(data.data.subscriptionsInfo.map((sub) => sub._id))
+      setIsSubscribed(
+        data.data.subscriptionsInfo
+          .map((sub) => sub._id)
+          // @ts-ignore
+          .includes(organizationId)
+      )
+    },
+  })
+
+  const subscribeMutation = useMutation({
+    mutationFn: subscribe,
+    onSuccess: async () => {
+      await subsQuery.refetch()
+    },
+  })
+
+  const unsubscribeMutation = useMutation({
+    mutationFn: unsubscribe,
+    onSuccess: async () => {
+      await subsQuery.refetch()
+    },
+  })
+
+  const { isFetching } = useQuery({
+    queryKey: ['organization', organizationId],
+    queryFn: getOrganizationById,
+    onSuccess: (data) => {
+      setIsSubscribed(subscriptions.includes(data.data._id))
+      setOrganization(data.data)
+    },
+    onError: (error: TypeError) => {
+      toast(error.message, {
+        type: 'error',
+      })
+    },
+    enabled: subsQuery.isFetched,
+  })
+
+  const toggleSubscription = () => {
+    if (!isSubscribed) subscribeMutation.mutate(organizationId)
+    else unsubscribeMutation.mutate(organizationId)
+  }
+
+  return (
+    <div className="flex justify-center">
+      {isFetching ? (
+        <ClipLoader size={150} color={colors.primary} />
+      ) : (
+        <div>
+          {organization?.name} {organization?.linkedin}
+          <Button
+            variant={isSubscribed ? 'info' : 'success'}
+            onClick={toggleSubscription}
+          >
+            {unsubscribeMutation.isLoading || subscribeMutation.isLoading ? (
+              <ClipLoader color={colors.white} />
+            ) : isSubscribed ? (
+              'Subscribed'
+            ) : (
+              'Subscribe'
+            )}
+          </Button>
+        </div>
+      )}
+    </div>
+  )
 }
