1164571867a8abac2bc0bd037da3c0d2bf441475
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-02-16 02:32:32 +0500

    feat: created unsubscription controller, optimized subscription controller

--- back/src/controllers/userControllers.ts
+++ back/src/controllers/userControllers.ts
@@ -4,21 +4,69 @@ import { MESSAGE } from '@/utils'
 
 export const getSubscriptionsByUID = async (req: Request, res: Response) => {
   try {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const subscriptions = await User.find({ _id: req?.user.userId }, 'subscriptions')
-    console.log(req?.user)
-    res.send(subscriptions)
+    const subscriptions = await User.aggregate([
+      {
+        $match: {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          _id: req.user._id,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          subscriptions: 1,
+        },
+      },
+      {
+        $lookup: {
+          from: 'organizations',
+          localField: 'subscriptions',
+          foreignField: '_id',
+          as: 'subscriptionsInfo',
+        },
+      },
+      {
+        $project: {
+          'subscriptionsInfo.name': 1,
+          'subscriptionsInfo._id': 1,
+        },
+      },
+    ])
+    res.send(subscriptions[0])
   } catch (e) {
-    res.send({ message: MESSAGE.DEFAULT_ERROR })
+    res.status(500).send({ message: MESSAGE.DEFAULT_ERROR })
   }
 }
 
 export const subscribe = async (req: Request, res: Response) => {
   try {
-    await User.updateOne({ _id: req.body.userId }, { $push: { subscriptions: req.body.organizationId } })
-    res.send({ message: MESSAGE.SUBSCRIBED })
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const userId = req.user._id
+    const user = await User.findOne({ _id: userId }, 'subscriptions')
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (user.subscriptions.includes(req.body.organizationId)) {
+      res.send({ message: MESSAGE.ALREADY_SUB })
+    } else {
+      await User.updateOne({ _id: userId }, { $push: { subscriptions: req.body.organizationId } })
+      res.send({ message: MESSAGE.SUBSCRIBED })
+    }
   } catch (e) {
-    res.send({ message: MESSAGE.DEFAULT_ERROR })
+    res.status(500).send({ message: MESSAGE.DEFAULT_ERROR })
+  }
+}
+
+export const unSubscribe = async (req: Request, res: Response) => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const userId = req.user._id
+    await User.updateOne({ _id: userId }, { $pull: { subscriptions: req.body.organizationId } })
+    res.send({ message: MESSAGE.UNSUBSCRIBED })
+  } catch (e) {
+    console.log(e)
+    res.status(500).send({ message: MESSAGE.DEFAULT_ERROR })
   }
 }
