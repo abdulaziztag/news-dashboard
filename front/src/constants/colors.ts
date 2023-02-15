413d90d2479cb65b3137c54eef97038386f5f8fb
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-02-16 02:04:02 +0500

    feat: moved colors into separate file

---
+++ front/src/constants/colors.ts
@@ -0,0 +1,13 @@
+export const colors: {
+  success: string
+  error: string
+  primary: string
+  info: string
+  white: string
+} = {
+  primary: '#4f46e5',
+  error: '#dc2626',
+  success: '#16a34a',
+  info: '#2563eb',
+  white: '#fff',
+}
