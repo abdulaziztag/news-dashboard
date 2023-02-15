346b9cf159acc0b35cefb9f68ecbbb9b5e8353d7
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-02-16 02:32:50 +0500

    feat: created unsubscription controller, optimized subscription controller

--- front/src/api/user.ts
+++ front/src/api/user.ts
@@ -1,6 +1,22 @@
-import axiosIntance from './axiosIntance'
+import axiosInstance from './axiosInstance'
 import { endpoints } from './endpoints'
+import { SubscriptionMini } from 'interfaces/IOrganization'
+import { SimpleMessageType } from 'types/SimpleMessageType'
 
 export const getSubscriptions = async () => {
-  return await axiosIntance.get(endpoints.subscriptions)
+  return await axiosInstance.get<{ subscriptionsInfo: SubscriptionMini[] }>(
+    endpoints.subscriptions
+  )
+}
+
+export const subscribe = async (organizationId?: string) => {
+  return await axiosInstance.post<SimpleMessageType>(endpoints.subscribe, {
+    organizationId,
+  })
+}
+
+export const unsubscribe = async (organizationId?: string) => {
+  return await axiosInstance.post<SimpleMessageType>(endpoints.unsubscribe, {
+    organizationId,
+  })
 }
