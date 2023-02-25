7f9b2349dddd3eeda7b8c3bf7ccd99f22286a818
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-02-25 15:54:08 +0500

    fix: changed primary color

--- front/src/components/Generic/Button/utils/variants.ts
+++ front/src/components/Generic/Button/utils/variants.ts
@@ -1,7 +1,7 @@
 import { ButtonVariant } from '../types'
 
 export const ButtonVariants: Record<ButtonVariant, string> = {
-  primary: 'bg-indigo-600 text-white hover:bg-indigo-700 focus:ring-indigo-500',
+  primary: 'bg-cyan-500 text-white hover:bg-cyan-600 focus:ring-cyan-400',
   success: 'bg-green-600 text-white hover:bg-green-700 focus:ring-green-500',
   info: 'bg-blue-600 text-white hover:bg-blue-700 focus:ring-blue-500',
   error: 'bg-red-600 text-white hover:bg-red-700 focus:ring-red-500',
--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -5,7 +5,7 @@ export const colors: {
   info: string
   white: string
 } = {
-  primary: '#4f46e5',
+  primary: '#0891b2',
   error: '#dc2626',
   success: '#16a34a',
   info: '#2563eb',
