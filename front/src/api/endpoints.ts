74e73621f360d30843845ed5ce66a05596ddbe1f
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-02-25 16:03:41 +0500

    fix: changed base url

--- front/src/api/endpoints.ts
+++ front/src/api/endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000/api'
+export const BASE_URL = 'https://rella.herokuapp.com/api'
 export const endpoints = {
   signIn: '/auth/signIn',
   signUp: '/auth/signUp',
