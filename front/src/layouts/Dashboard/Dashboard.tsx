2ecdcf12207b5306023838e626389307c82c449b
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-02-25 15:54:55 +0500

    fix: style bugs

--- front/src/layouts/Dashboard/Dashboard.tsx
+++ front/src/layouts/Dashboard/Dashboard.tsx
@@ -1,34 +1,21 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
-import { Outlet, useNavigate, useParams } from 'react-router-dom'
+import { Outlet, useParams } from 'react-router-dom'
 import { DesktopNav } from './components/DesktopNav'
 import { MobileNav } from './components/MobileNav'
 import { useQuery } from '@tanstack/react-query'
 import { getSubscriptions } from 'api/user'
 import { SubscriptionMini } from 'interfaces/IOrganization'
 import { toast } from 'react-toastify'
-import { checkAuth } from 'api/auth'
+import { AxiosError } from 'axios'
+import { useDocumentTitle } from 'hooks'
 
 export const Dashboard = () => {
   const { organizationId } = useParams()
-  const navigate = useNavigate()
   const [sidebarOpen, setSidebarOpen] = useState(false)
   const [subscription, setSubscription] = useState<SubscriptionMini[]>([])
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
 
-  useQuery({
-    queryKey: ['checkAuth'],
-    queryFn: checkAuth,
-    onSuccess: () => {
-      setIsLoggedIn(true)
-    },
-    onError: () => {
-      toast('Please sign in', {
-        type: 'error',
-      })
-      navigate('/auth/signin')
-    },
-  })
+  useDocumentTitle('Dashboard • Rella')
 
   const { isLoading, isRefetching } = useQuery({
     queryKey: ['subscriptions'],
@@ -36,12 +23,13 @@ export const Dashboard = () => {
     onSuccess: (data) => {
       setSubscription(data.data.subscriptionsInfo)
     },
-    onError: (error: TypeError) => {
-      toast(error.message, {
-        type: 'error',
-      })
+    onError: (error: AxiosError) => {
+      error.response &&
+        error.response.status !== 401 &&
+        toast(error.message, {
+          type: 'error',
+        })
     },
-    enabled: isLoggedIn,
   })
 
   return (
@@ -61,13 +49,7 @@ export const Dashboard = () => {
         />
         <div className="flex flex-1 flex-col md:pl-64">
           <Header setSidebarOpen={setSidebarOpen} />
-          <main className="flex-1 py-6 mx-auto max-w-[86rem] px-4 sm:px-6 md:px-8">
-            {/* Don't touch it, it's my signature :) */}
-            <p className="text-white">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Accusamus aperiam dignissimos fuga illum labore laborum modi
-              officiis voluptas? Amet corporis cumque ducimus illo obcaecati
-            </p>
+          <main className="flex py-6 px-4 sm:px-6 md:px-12 lg:px-24 xl:px-32 2xl:px-52">
             <Outlet />
           </main>
         </div>
