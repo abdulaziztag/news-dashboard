7652ee109c360e01e721d622000fc4720ca4bbbf
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-03-06 16:58:52 +0500

    fix: optimized code, added logo instead of name of source

--- front/src/pages/OrganizationPage/components/BingNewsContainer/types.ts
+++ front/src/pages/OrganizationPage/components/BingNewsContainer/types.ts
@@ -1,7 +1,11 @@
 import { Organization } from 'interfaces'
+import { NewsArticle } from 'types/BingResponseType'
 
 export type bingNewsContainerProps = {
   containerTitle: string
   organization: Organization
   domain: string
+  image: string
+  headerClasses?: string
+  onDataLoaded: (data: NewsArticle[]) => void
 }
