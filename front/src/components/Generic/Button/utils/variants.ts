f7ff57e1a7ed33255d8cb31c74e43fa218ec59a8
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-03-06 16:47:54 +0500

    style: fixed ligth variant of button

--- front/src/components/Generic/Button/utils/variants.ts
+++ front/src/components/Generic/Button/utils/variants.ts
@@ -5,5 +5,6 @@ export const ButtonVariants: Record<ButtonVariant, string> = {
   success: 'bg-green-600 text-white hover:bg-green-700 focus:ring-green-500',
   info: 'bg-blue-600 text-white hover:bg-blue-700 focus:ring-blue-500',
   error: 'bg-red-600 text-white hover:bg-red-700 focus:ring-red-500',
-  light: 'bg-white text-black hover:bg-gray-200 focus:ring-white',
+  light:
+    'bg-white text-black hover:bg-gray-200 focus:ring-white border-2 border-gray-400',
 }
