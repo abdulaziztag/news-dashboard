69d8385158c414a41d5e18bc9b710a18d1aa87a0
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-03-08 00:27:19 +0500

    feat: added reminders interface

---
+++ front/src/interfaces/IReminder.ts
@@ -0,0 +1,8 @@
+export interface IReminder {
+  notes?: string
+  expiresDate: number
+  organizationId: string
+  repeatValue?: number
+  title?: string
+  _id?: string
+}
--- front/src/interfaces/index.ts
+++ front/src/interfaces/index.ts
@@ -1,2 +1,3 @@
 export * from './IOrganization'
 export * from './IUser'
+export * from './IReminder'
