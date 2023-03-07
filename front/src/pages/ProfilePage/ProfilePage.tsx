74a2c4a54008c351374de8842c7a03f04e283ceb
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-03-08 00:42:18 +0500

    feat: added today's reminders in profile page

--- front/src/pages/ProfilePage/ProfilePage.tsx
+++ front/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,31 +1,52 @@
+import { useCheckAuth } from 'hooks'
 import { useQuery } from '@tanstack/react-query'
-import { checkAuth } from 'api/auth'
-import { toast } from 'react-toastify'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { IUser } from 'interfaces'
+import { getReminders } from 'api/user'
+import { Link } from 'react-router-dom'
+import { routePaths } from 'router/routes'
+import { RemindersList } from 'components/RemindersList'
+import { getCorrectTimezone } from 'helpers/getCorrectTimezone'
 
+const noRemindersText = (
+  <>
+    There is no follow-ups today. Go to{' '}
+    <Link className="font-bold" to={routePaths.reminders}>
+      follow-ups
+    </Link>{' '}
+    page to see more
+  </>
+)
 export const ProfilePage = () => {
-  const navigate = useNavigate()
-
-  const [user, setUser] = useState<IUser>()
-
-  useQuery({
-    queryKey: ['checkAuth'],
-    queryFn: checkAuth,
-    onSuccess: (data) => {
-      setUser(data.data)
-    },
-    onError: () => {
-      toast('Please sign in', {
-        type: 'error',
-      })
-      navigate('/auth/signin')
-    },
+  const { user } = useCheckAuth()
+  const { data, isLoading, isRefetching, refetch } = useQuery({
+    queryKey: ['reminders'],
+    queryFn: getReminders,
+    staleTime: Infinity,
   })
+
   return (
-    <div className="border-2 border-red-600 w-full">
+    <div className="w-full">
       Profile {user?.email} {user?.firstName} {user?.lastName}
+      <div className="flex flex-col gap-y-5">
+        <h2 className="text-3xl font-semibold mt-5">Follow-ups for today</h2>
+        <div className="flex items-center flex-col gap-y-5">
+          <RemindersList
+            reminders={
+              (data &&
+                data.data.filter((reminder) => {
+                  const correctedTimezone = getCorrectTimezone(
+                    reminder.expiresDate
+                  )
+                  const date = new Date(correctedTimezone)
+                  return date.getDate() === new Date().getDate()
+                })) ||
+              []
+            }
+            noRemindersText={noRemindersText}
+            loader={isLoading || isRefetching}
+            refetch={refetch}
+          />
+        </div>
+      </div>
     </div>
   )
 }
