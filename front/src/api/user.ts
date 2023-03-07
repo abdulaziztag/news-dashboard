0e467f70deb27171f13e7ef5504a389063990c57
Abdulaziz <abdulazizmannopov07@gmail.com>
2023-03-08 00:44:30 +0500

    fix: CRD for reminders

--- front/src/api/user.ts
+++ front/src/api/user.ts
@@ -2,6 +2,7 @@ import axiosInstance from './axiosInstance'
 import { endpoints } from './endpoints'
 import { SubscriptionMini } from 'interfaces/IOrganization'
 import { SimpleMessageType } from 'types/SimpleMessageType'
+import { IReminder } from '../interfaces'
 
 export const getSubscriptions = async () => {
   return await axiosInstance.get<{ subscriptionsInfo: SubscriptionMini[] }>(
@@ -18,5 +19,30 @@ export const subscribe = async (organizationId?: string) => {
 export const unsubscribe = async (organizationId?: string) => {
   return await axiosInstance.post<SimpleMessageType>(endpoints.unsubscribe, {
     organizationId,
+  })
+}
+
+export const setReminder = async (reminder: IReminder) => {
+  return await axiosInstance.post<SimpleMessageType>(
+    endpoints.reminder,
+    reminder
+  )
+}
+
+export const getReminders = async () => {
+  return await axiosInstance.get<
+    (IReminder & {
+      organizationInfo: {
+        name: string
+      }
+    })[]
+  >(endpoints.reminder)
+}
+
+export const deleteReminder = async (reminderId: string) => {
+  return await axiosInstance.delete<SimpleMessageType>(endpoints.reminder, {
+    data: {
+      reminderId,
+    },
   })
 }
